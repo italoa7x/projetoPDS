a2783b9508532d0fa9ede5e919077bef74866331
Italo <43725339+italoa7x@users.noreply.github.com>
2019-11-13 10:46:51 -0300

    Revert "Dev"

--- CointerProject/app/src/main/java/br/com/cointerproject/model/exceptions/ErroAoLogarException.java
+++
@@ -1,7 +0,0 @@
-package br.com.cointerproject.model.exceptions;
-
-public class ErroAoLogarException extends Exception {
-    public ErroAoLogarException() {
-        super("Usuário não encontrado!");
-    }
-}
--- CointerProject/app/src/main/java/br/com/cointerproject/model/exceptions/UsuarioNaoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package br.com.cointerproject.model.exceptions;
-
-public class UsuarioNaoEncontradoException extends Exception {
-    public UsuarioNaoEncontradoException(){
-        super("Usuário não encontrado.");
-    }
-}
