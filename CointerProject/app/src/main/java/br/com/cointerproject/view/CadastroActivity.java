49b18e79e3cf778a078a23bfe29992bff979d377
peagcarvalho <peagcarvalho@users.noreply.github.com>
2019-11-18 15:55:22 -0300

    Criação da tela de cadastro
    Foi criada a tela da funcionalidade #1 (Cadastro de usuário), com nome, data de nascimento, e-mail e senha.

---
+++ CointerProject/app/src/main/java/br/com/cointerproject/view/CadastroActivity.java
@@ -0,0 +1,14 @@
+package br.com.cointerproject.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import br.com.cointerproject.R;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
