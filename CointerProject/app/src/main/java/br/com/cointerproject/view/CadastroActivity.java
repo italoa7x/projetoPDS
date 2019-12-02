e7c10a50fa6f5f6f31f94374fe82e786764ac959
peagcarvalho <peagcarvalho@users.noreply.github.com>
2019-12-02 00:39:41 -0300

    Update CadastroActivity.java

--- CointerProject/app/src/main/java/br/com/cointerproject/view/CadastroActivity.java
+++ CointerProject/app/src/main/java/br/com/cointerproject/view/CadastroActivity.java
@@ -2,26 +2,18 @@ package br.com.cointerproject.view;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.graphics.Color;
 import android.graphics.PorterDuff;
-import android.graphics.drawable.Icon;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
-
 import br.com.cointerproject.R;
 import br.com.cointerproject.model.Validacao;
 
