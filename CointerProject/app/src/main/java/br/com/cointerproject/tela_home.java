f7251786587e985a2c0610c8828c98374f2e8ccd
italo <contato.italo1020@gmail.com>
2019-12-08 23:13:54 -0300

    Mudando para o menu lateral. FOi decidido murar para o menu lateral, pois o dashboard possibilitava apenas 5 itens no menu.

---
+++ CointerProject/app/src/main/java/br/com/cointerproject/tela_home.java
@@ -0,0 +1,14 @@
+package br.com.cointerproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tela_home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_home);
+    }
+}
--- CointerProject/app/src/main/java/br/com/cointerproject/ui/fragments_telas/FragmentTelaHome.java
+++ CointerProject/app/src/main/java/br/com/cointerproject/ui/fragments_telas/FragmentTelaHome.java
@@ -1,4 +1,4 @@
-package br.com.cointerproject.ui.fragments_dashboard;
+package br.com.cointerproject.ui.fragments_telas;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,13 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import br.com.cointerproject.R;
-
-public class FragmentListarInvestimentos extends Fragment {
+public class FragmentTelaHome extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_lista_investimentos, container, false);
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
