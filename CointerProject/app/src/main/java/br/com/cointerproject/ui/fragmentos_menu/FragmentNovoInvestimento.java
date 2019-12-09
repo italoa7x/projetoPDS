95d9bc91bc6259e8d627c9686202c8d7cfb33c09
italo <contato.italo1020@gmail.com>
2019-12-09 17:22:38 -0300

    Menu alterado para o componente fornecido pelo android studio

--- CointerProject/app/src/main/java/br/com/cointerproject/ui/fragmentos_menu/FragmentNovoInvestimento.java
+++ CointerProject/app/src/main/java/br/com/cointerproject/ui/fragmentos_menu/FragmentNovoInvestimento.java
@@ -1,4 +1,4 @@
-package br.com.cointerproject.ui.fragments_telas;
+package br.com.cointerproject.ui.fragmentos_menu;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,11 +9,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class FragmentTelaHome extends Fragment {
+import br.com.cointerproject.R;
+
+public class FragmentNovoInvestimento extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.activity_cadastro_investimento, container, false);
     }
 }
