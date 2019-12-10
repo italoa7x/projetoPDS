21485a3af19871aead3fa3b01a8e55f00c8660c1
italo <contato.italo1020@gmail.com>
2019-12-10 12:15:01 -0300

    Ajustes na transiçãao de telas do menu

--- CointerProject/app/src/main/java/br/com/cointerproject/TelaHome.java
+++ CointerProject/app/src/main/java/br/com/cointerproject/TelaHome.java
@@ -2,32 +2,27 @@ package br.com.cointerproject;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import android.view.MenuItem;
-import android.view.View;
-
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.core.view.GravityCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
+
+import br.com.cointerproject.ui.fragmentos.FragmentoListarInvestimentos;
+import br.com.cointerproject.ui.fragmentos.FragmentNovoInvestimento;
+import br.com.cointerproject.ui.fragmentos.FragmentoHome;
+import br.com.cointerproject.ui.fragmentos.FragmentoValoresMonetarios;
 import br.com.cointerproject.ui.login.TelaLogin;
-import br.com.cointerproject.view.CadastroInvActivity;
-import br.com.cointerproject.view.ListaInvestimentos;
-import br.com.cointerproject.view.TelaConsultaValores;
 
-public class TelaHome extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private AppBarConfiguration mAppBarConfiguration;
+public class TelaHome extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,27 +30,22 @@ public class TelaHome extends AppCompatActivity implements NavigationView.OnNavi
         setContentView(R.layout.activity_tela_home);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.op_home, R.id.op_perfil, R.id.op_meus_investimentos, R.id.op_novo_investimento,
-                R.id.op_valores_monetarios, R.id.op_logof)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
-        navigationView.setNavigationItemSelectedListener(this);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigation = findViewById(R.id.nav_view);
+        navigation.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toogler = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.abrir_navegacao, R.string.fechar_navegacao);
+
+        drawer.addDrawerListener(toogler);
+        toogler.syncState();
+
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,
+                    new FragmentoValoresMonetarios()).commit();
+            navigation.setCheckedItem(R.id.op_valores_monetarios);
+        }
     }
 
     @Override
@@ -64,14 +54,6 @@ public class TelaHome extends AppCompatActivity implements NavigationView.OnNavi
         getMenuInflater().inflate(R.menu.tela_home, menu);
         return true;
     }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
-
 
 
     @Override
@@ -84,42 +66,33 @@ public class TelaHome extends AppCompatActivity implements NavigationView.OnNavi
         }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings){
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        int id = menuItem.getItemId();
-        if(id == R.id.op_meus_investimentos){
-            Intent it = new Intent(TelaHome.this, ListaInvestimentos.class);
-            startActivity(it);
-        }
-        else if (id == R.id.op_novo_investimento){
-            Intent it = new Intent(TelaHome.this, CadastroInvActivity.class);
-            startActivity(it);
-        }
-        else if (id == R.id.op_valores_monetarios){
-            Intent it = new Intent(TelaHome.this, TelaConsultaValores.class);
-            startActivity(it);
-        }
-        else if (id == R.id.op_home){
+        switch(menuItem.getItemId()){
+            case R.id.op_novo_investimento:
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,
+                        new FragmentNovoInvestimento()).commit();
+                break;
+            case R.id.op_valores_monetarios:
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,
+                        new FragmentoValoresMonetarios()).commit();
+                break;
+
+
+            case R.id.op_meus_investimentos:
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,
+                        new FragmentoListarInvestimentos()).commit();
+                break;
+
+            case R.id.op_logof:
+                FirebaseAuth.getInstance().signOut();
+                Intent it = new Intent(TelaHome.this, TelaLogin.class);
+                startActivity(it);
+                break;
 
         }
-        else if (id == R.id.op_perfil){
-
-        }
-        else if (id == R.id.op_logof){
-            Intent it = new Intent(TelaHome.this, TelaLogin.class);
-            startActivity(it);
-        }
-        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawerLayout.closeDrawer(GravityCompat.START);
+        drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 }
