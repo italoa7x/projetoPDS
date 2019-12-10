4dc9a4c978c6c53231dee35d20fe87a3c7b04419
ferrazandre <55859517+ferrazandre@users.noreply.github.com>
2019-12-10 09:43:56 -0300

    Interações da barra de menu

--- CointerProject/app/src/main/java/br/com/cointerproject/TelaHome.java
+++ CointerProject/app/src/main/java/br/com/cointerproject/TelaHome.java
@@ -1,27 +1,31 @@
 package br.com.cointerproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-
 import com.google.android.material.navigation.NavigationView;
-
 import androidx.drawerlayout.widget.DrawerLayout;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.view.Menu;
+import br.com.cointerproject.ui.login.TelaLogin;
+import br.com.cointerproject.view.CadastroInvActivity;
+import br.com.cointerproject.view.ListaInvestimentos;
+import br.com.cointerproject.view.TelaConsultaValores;
 
-public class TelaHome extends AppCompatActivity {
+public class TelaHome extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private AppBarConfiguration mAppBarConfiguration;
 
@@ -51,6 +55,7 @@ public class TelaHome extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -65,5 +70,56 @@ public class TelaHome extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        if(id == R.id.op_meus_investimentos){
+            Intent it = new Intent(TelaHome.this, ListaInvestimentos.class);
+            startActivity(it);
+        }
+        else if (id == R.id.op_novo_investimento){
+            Intent it = new Intent(TelaHome.this, CadastroInvActivity.class);
+            startActivity(it);
+        }
+        else if (id == R.id.op_valores_monetarios){
+            Intent it = new Intent(TelaHome.this, TelaConsultaValores.class);
+            startActivity(it);
+        }
+        else if (id == R.id.op_home){
+
+        }
+        else if (id == R.id.op_perfil){
+
+        }
+        else if (id == R.id.op_logof){
+            Intent it = new Intent(TelaHome.this, TelaLogin.class);
+            startActivity(it);
+        }
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
